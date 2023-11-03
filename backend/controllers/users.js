f3bc20a8c91370c1bc604136c29c6d8d3cfa0725
Alexander Rasyuk <th3arsk@yandex.ru>
2023-11-03 17:34:01 +0300

    mesto 1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -30,6 +30,7 @@ const createUser = (req, res, next) => {
             about: user.about,
             avatar: user.avatar,
             email: user.email,
+            _id: user._id,
           },
         }))
         .catch((err) => {
